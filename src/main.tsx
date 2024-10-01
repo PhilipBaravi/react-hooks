551d871f68c8beb057dcc14f08b79b74744dbfc2
Philip <90202154+PhilipBaravi@users.noreply.github.com>
2024-10-01 15:37:26 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+
+const baseUrl = import.meta.env.BASE_URL || '/react-hooks/';
+const link = document.createElement('link');
+link.rel = 'icon';
+link.type = 'image/svg+xml';
+link.href = `${baseUrl}hook.svg?v=1.1`;
+
+document.head.appendChild(link);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
